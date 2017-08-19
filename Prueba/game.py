5a515e8018176aa305d0db4551fade89ee0dfe48
reypala <lucastisera@gmail.com>
2017-08-19 18:23:16 -0300

    Mod en Velocidad
    Cambie el modo en que se leen las teclas

--- Prueba/game.py
+++ Prueba/game.py
@@ -1,16 +1,19 @@
 import pygame, sys, math
-
+from pygame.locals import *    # Nuevo en la version 0.03
 pygame.init()
 
 pantalla = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Holis")
+pygame.display.set_caption("ASSG")
 
-nave = pygame.image.load("Chico.png")
+nave = pygame.image.load("Recursos/Chico.png")
 nave = pygame.transform.rotate(nave, 270)
-planeta1 = pygame.image.load("Planeta.png")
-planeta12 = pygame.image.load("Planeta2.png")
-fondo1 = pygame.image.load("1.png")
-fondo2 = pygame.image.load("2.png")
+
+planeta1 = pygame.image.load("Recursos/Planeta.png")
+planeta12 = pygame.image.load("Recursos/Planeta2.png")
+
+
+fondo1 = pygame.image.load("Recursos/1.png")
+fondo2 = pygame.image.load("Recursos/2.png")
 moverX = 0
 moverY = 0
 X = 0
@@ -27,61 +30,41 @@ while intro:
 	pantalla.fill((255,255,255))
 	
 	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == K_ESCAPE:
+				pygame.quit()
 		if event.type == pygame.QUIT:
 			pygame.quit()
 	
+	#Lectura de TECLAS
+	keys = pygame.key.get_pressed()
+	if keys[K_w]:
+		angulo +=2
+		if angulo >= 360:
+			angulo -= 360
+		elif angulo < 0:
+			angulo += 360	
+		moverY = math.sin(math.radians(angulo))*velocidad
+		moverX = math.cos(math.radians(angulo))*velocidad
 		
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_w:
-				angulo += 10
-			if event.key == pygame.K_s:
-				angulo -= 10
-			if event.key == pygame.K_m:
-				velocidad += 0.5
-			if event.key == pygame.K_n:
-				velocidad -= 0.25	
-			if angulo >= 360:
-				angulo -= 360
-			elif angulo < 0:
-				angulo += 360	
-			moverY = math.sin(math.radians(angulo))*velocidad
-			moverX = math.cos(math.radians(angulo))*velocidad
-			print("Y:", moverY)
-			print("X:", moverX)
-			print("Angulo:", angulo)
-		
+	if keys[K_s]:
+		angulo -=2
+		if angulo >= 360:
+			angulo -= 360
+		elif angulo < 0:
+			angulo += 360	
+		moverY = math.sin(math.radians(angulo))*velocidad
+		moverX = math.cos(math.radians(angulo))*velocidad
 	
-	"""	
-		if event.type == pygame.KEYDOWN:
-			tiempo = 0
-			if event.key == pygame.K_LEFT:
-				velocidad += aceleracion * tiempo 
-				moverX += velocidad
-			elif event.key == pygame.K_RIGHT:
-				velocidad += aceleracion * tiempo 
-				moverX -= velocidad
-			elif event.key == pygame.K_UP:
-				velocidad += aceleracion * tiempo 
-				moverY += velocidad
-			elif event.key == pygame.K_DOWN:
-				velocidad += aceleracion * tiempo 
-				moverY -= velocidad
-		if event.type == pygame.KEYUP:
-			tiempo = 0
-			if event.key == pygame.K_LEFT:
-				velocidad -= aceleracion * tiempo 
-				moverX = velocidad
-			elif event.key == pygame.K_RIGHT:
-				velocidad -= aceleracion * tiempo 
-				moverX = -velocidad
-			elif event.key == pygame.K_UP:
-				velocidad -= aceleracion * tiempo 
-				moverY = velocidad
-			elif event.key == pygame.K_DOWN:
-				velocidad -= aceleracion * tiempo 
-				moverY = -velocidad
-	"""
-			
+	if keys[K_m]:
+		velocidad += 1
+		moverY = math.sin(math.radians(angulo))*velocidad
+		moverX = math.cos(math.radians(angulo))*velocidad
+	if keys[K_n]:
+		velocidad -= 1
+		moverY = math.sin(math.radians(angulo))*velocidad
+		moverX = math.cos(math.radians(angulo))*velocidad
+
 	X += moverX
 	Y += moverY
 	
