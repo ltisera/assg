7c737887d0debca22ec75bf123bfb528afbea6c8
cmathov <31076435+cmathov@users.noreply.github.com>
2017-08-20 22:54:28 -0300

    Intento de Clases
    Andar anda, no se si sea la mejor forma de dividir las clases pero fue la que se me ocurrio en el momento XD
    Si alguien quiere cambiarlo es libre de hacerlo :P Y si no entienden que significa algo preguntenme

--- Prueba/game.py
+++ Prueba/game.py
@@ -1,43 +1,102 @@
 import pygame, sys, math
-from pygame.locals import *    # Nuevo en la version 0.03
+from pygame.locals import *    # Intento de clases
 pygame.init()
 
-pantalla = pygame.display.set_mode((800,600))
-pygame.display.set_caption("ASSG")
+class Nave:
+	def sumarAngulo(self, sumarAngulo):
+		self.angulo += sumarAngulo
+		if self.angulo >= 360:
+			self.angulo -= 360
+		elif self.angulo < 0:
+			self.angulo += 360
+		self.mover()
+	def sumarVelocidad(self, sumar):
+		self.velocidad = self.velocidad + sumar
+		if self.velocidad <= (self.minVelocidad):
+			self.velocidad = self.minVelocidad
+		elif self.velocidad >= (self.maxVelocidad):
+			self.velocidad = self.maxVelocidad
+		self.mover()
+	def mover(self):
+		self.X = math.cos(math.radians(self.angulo))*self.velocidad
+		self.Y = math.sin(math.radians(self.angulo))*self.velocidad
+	def imprimir(self, centroX, centroY):
+		pantalla.display.blit(pygame.transform.rotate(self.imagen, self.angulo), (centroX-38,centroY-27))
+	def __init__(self, directorio, anguloOrigen, velocidad, minVelocidad, maxVelocidad):
+		self.imagen = pygame.image.load(directorio)
+		self.imagen = pygame.transform.rotate(self.imagen, anguloOrigen)
+		self.X = 0
+		self.Y = 0
+		self.maxVelocidad = maxVelocidad
+		self.minVelocidad = minVelocidad
+		self.angulo = 0
+		self.velocidad = 0
+		self.sumarVelocidad(velocidad)
 
-nave = pygame.image.load("Recursos/Chico.png")
-nave = pygame.transform.rotate(nave, 270)
+class Pantalla:
+	def imprimir(self):
+		#pantalla.display.blit(self.fondo1, (700,0))
+		pantalla.display.blit(self.fondo1, (650,0))
+		pantalla.display.blit(self.fondo2, (0,475))
+	def mover(self, X, Y):
+		self.X += X
+		self.Y += Y
+	def __init__(self):
+		self.display = pygame.display.set_mode((800,600))
+		pygame.display.set_caption("ASSG")
+		self.X = 0
+		self.Y = 0
+		self.fondo1 = pygame.image.load("Recursos/1.png")
+		self.fondo2 = pygame.image.load("Recursos/2.png")
+		self.centroX = 325
+		self.centroY = 237.5
 
-enemigo = pygame.image.load("Recursos/Enemigo.png")
+class Enemigo:
+	def mover(self, X, Y, centroX, centroY):
+		self.X -= X
+		self.Y += Y
+		if self.X < centroX-50:
+			self.X += self.velocidad
+		elif self.X > centroX-50:
+			self.X -= self.velocidad
+		if self.Y < centroY-28:
+			self.Y += self.velocidad
+		elif self.Y > centroY-28:
+			self.Y -= self.velocidad
+	def imprimir(self, X, Y, centroX, centroY):
+		self.mover(X, Y, centroX, centroY)
+		pantalla.display.blit(self.imagen, (self.X,self.Y))
+	def __init__(self, directorio, X, Y, velocidad):
+		self.imagen = pygame.image.load(directorio)
+		self.X = X
+		self.Y = Y
+		self.velocidad = velocidad
 
-planeta1 = pygame.image.load("Recursos/Planeta.png")
-planeta12 = pygame.image.load("Recursos/Planeta2.png")
+class Planeta:
+	def imprimir(self, X, Y):
+		pantalla.display.blit(self.imagen, (-X+self.origenX,Y+self.origenY))
+	def __init__(self, directorio, origenX, origenY):
+		self.imagen = pygame.image.load(directorio)
+		self.origenX = origenX
+		self.origenY = origenY
 
+pantalla = Pantalla()		
+		
+nave = Nave("Recursos/Chico.png", 270, 0.1, 0.1, 20)
 
-fondo1 = pygame.image.load("Recursos/1.png")
-fondo2 = pygame.image.load("Recursos/2.png")
-moverX = 0
-moverY = 0
-X = 0
-Y = 0
+enemigo = Enemigo("Recursos/Enemigo.png", 200, 100, 2)
+
+planeta1 = Planeta("Recursos/Planeta.png", 10,10)
+planeta2 = Planeta("Recursos/Planeta2.png", 300,350)
+planeta3 = Planeta("Recursos/Planeta2.png", -500,100)
+
 intro = True
-aceleracion = 1
-velocidad = 0.1
-angulo = 0
+
 clock = pygame.time.Clock()
-tiempo = 0
-
-# Origen y velocidad del enemigo
-XE = 200
-YE = 100
-velocidadE = 2
-
-moverY = math.sin(math.radians(angulo))*velocidad
-moverX = math.cos(math.radians(angulo))*velocidad
 		
 while intro:
 
-	pantalla.fill((229,221,213))
+	pantalla.display.fill((229,221,213))
 	
 	for event in pygame.event.get():
 		if event.type == pygame.KEYDOWN:
@@ -49,67 +108,28 @@ while intro:
 	#Lectura de TECLAS
 	keys = pygame.key.get_pressed()
 	if keys[K_w]:
-		angulo +=2 
-		if angulo >= 360:
-			angulo -= 360
-		elif angulo < 0:
-			angulo += 360	
-		moverY = math.sin(math.radians(angulo))*velocidad
-		moverX = math.cos(math.radians(angulo))*velocidad
+		nave.sumarAngulo(2)
 		
 	if keys[K_s]:
-		angulo -=2
-		if angulo >= 360:
-			angulo -= 360
-		elif angulo < 0:
-			angulo += 360	
-		moverY = math.sin(math.radians(angulo))*velocidad
-		moverX = math.cos(math.radians(angulo))*velocidad
-	
+		nave.sumarAngulo(-2)
+		
 	if keys[K_m]:
-		if velocidad >= 20:
-			velocidad = 20
-		else:
-			velocidad += 0.1
-		moverY = math.sin(math.radians(angulo))*velocidad
-		moverX = math.cos(math.radians(angulo))*velocidad
-	if keys[K_n]:
-		if velocidad <= 0:
-			velocidad = 0.1
-		else:
-			velocidad -= 0.5
-		if velocidad <= 0:
-			velocidad = 0.1
-		
-		moverY = math.sin(math.radians(angulo))*velocidad
-		moverX = math.cos(math.radians(angulo))*velocidad
+		nave.sumarVelocidad(0.1)
 
-	X += moverX
-	Y += moverY
+	if keys[K_n]:
+		nave.sumarVelocidad(-0.5)
 	
-	#Movimiento del enemigo
-	XE -= moverX
-	YE += moverY
-	
-	if XE < 225:
-		XE += velocidadE
-	elif XE > 225:
-		XE -= velocidadE
-		
-	if YE	< 300:
-		YE += velocidadE
-	elif YE > 300:
-		YE -= velocidadE
-	
-	pantalla.blit(planeta1, (-X+10,Y+10))
-	pantalla.blit(planeta12, (-X+300,Y+350))
-	pantalla.blit(planeta12, (-X-500,Y+100))
-	pantalla.blit(pygame.transform.rotate(nave, angulo), (225,300))
-	pantalla.blit(enemigo, (XE,YE))
-#	pantalla.blit(fondo1, (700,0))
-	pantalla.blit(fondo1, (650,0))
-	pantalla.blit(fondo2, (0,475))
+	#Impresion
+	pantalla.mover(nave.X, nave.Y)
+	planeta1.imprimir(pantalla.X, pantalla.Y)
+	planeta2.imprimir(pantalla.X, pantalla.Y)
+	planeta3.imprimir(pantalla.X, pantalla.Y)
+	nave.imprimir(pantalla.centroX, pantalla.centroY)
+	enemigo.imprimir(nave.X, nave.Y, pantalla.centroX, pantalla.centroY)
+	pantalla.imprimir()
 	pygame.display.update()
+	
+	
 	clock.tick(30)
 	#print(clock)
-	tiempo += 1
+
